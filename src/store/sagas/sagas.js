72e9de40292f579493ca320db33af1c161183440
Adam Siekierski <a@siekierski.ml>
2020-10-10 21:12:53 +0200

    Save which episode was played recently, so when opening the app, it automatically appears in the player

--- src/store/sagas/sagas.js
+++ src/store/sagas/sagas.js
@@ -1,10 +1,10 @@
 // @flow
 import { takeEvery, put, all } from 'redux-saga/effects';
-import AsyncStorage from '@react-native-community/async-storage';
 import SplashScreen from 'react-native-splash-screen';
 import { episodes as episodesAction } from '../actions/episodes';
 import { Episode } from '../../types';
 import { playEpisode } from '../../player';
+import EpisodeProgressService from '../../services/EpisodeProgressService';
 
 function* getEpisodes() {
   const { episodes }: { episodes: Episode[] } = yield fetch(
@@ -13,13 +13,7 @@ function* getEpisodes() {
 
   yield put(episodesAction(episodes));
 
-  yield AsyncStorage.getItem('last_playing')
-    .then((id) => {
-      if (id) {
-        playEpisode(id, false);
-      }
-    })
-    .catch(() => {});
+  yield EpisodeProgressService.lastPlaying.get().then((id) => id && playEpisode(id, false));
 
   SplashScreen.hide();
 }
